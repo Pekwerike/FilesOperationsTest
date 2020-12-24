bbf097cf981e620d7c67ce733f4156d3c299f5c7
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-24 18:03:56 +0100

    Codestina

--- app/src/main/java/com/salesground/androidtesting/FilesUtils.kt
+++ app/src/main/java/com/salesground/androidtesting/FilesUtils.kt
@@ -3,33 +3,34 @@ package com.salesground.androidtesting
 import android.content.Context
 import java.io.File
 
-class FilesUtils(private val context: Context) {
 
-    fun createFile(fileName: String): File {
+class FilesUtils(private val context: Context) : IFilesUtils {
+
+    override fun createFile(fileName: String): File {
         val folder = getMainFolder()
         val newFile = File(folder, fileName)
         if(!newFile.exists()) newFile.createNewFile()
         return newFile
     }
 
-    fun deleteFile(file: File) {
+    override fun deleteFile(file: File) {
         file.delete()
     }
 
-    fun renameFile(file: File, newFileName: String): File {
+    override fun renameFile(file: File, newFileName: String): File {
         val folder = getMainFolder()
         val newFile = File(folder, newFileName)
         file.renameTo(newFile)
         return newFile
     }
 
-    fun getMainFolder(): File {
+    override fun getMainFolder(): File {
         val folder = File(context.getExternalFilesDir(null), "MainFolder")
         if (!folder.exists()) folder.mkdirs()
         return folder
     }
 
-    fun deleteMainDirectory(): List<File> {
+    override fun deleteMainDirectory(): List<File> {
         val mainFolder = getMainFolder()
         var filesInMainFolder: List<File> = listOf()
         mainFolder.listFiles()?.sortedBy {
---
+++ app/src/main/java/com/salesground/androidtesting/IFilesUtils.kt
@@ -0,0 +1,14 @@
+package com.salesground.androidtesting
+
+import java.io.File
+
+interface IFilesUtils {
+
+    fun createFile(fileName: String): File
+    fun deleteFile(file: File)
+    fun renameFile(file: File, newFileName: String): File
+    fun getMainFolder(): File
+    fun deleteMainDirectory(): List<File>
+}
+
+g
